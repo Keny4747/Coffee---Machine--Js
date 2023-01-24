6f672c3160f7bba5cf980875e99957d06495e610
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-01-24 11:02:34 -0500

    ...

--- machine.js
+++ machine.js
@@ -2,6 +2,9 @@
 // Use "input(str)" to print some text before requesting input
 // You will need this in the following stages
 const input = require("sync-input");
+const water = 200;
+const milk = 50;
+const beans = 15;
 
 function starting() {
   console.log(`Starting to make a coffee\nGrinding coffee beans\nBoiling water\nMixing boiled water with crushed coffee beans
@@ -15,13 +18,22 @@ function inputUser() {
   return user;
 }
 function calculateCoffe(inputUser) {
-  const water = 200;
-  const milk = 50;
-  const beans = 15;
+
   console.log(`For ${inputUser} cups of coffee you will need:
 ${inputUser * water} ml of water
 ${inputUser * milk} ml of milk
 ${inputUser * beans} g of coffee beans`);
+}
+function coffeeAvailable(){
+    console.log("Write how many ml of water the coffee machine has:");
+    let userWater=Number(input());
+    console.log("Write how many ml of milk the coffee machine has:");
+    let userMilk=Number(input());
+    console.log("Write how many grams of coffee beans the coffee machine has:");
+    let userBeans=Number(input());
+    console.log("Write how many cups of coffee you will need:");
+    let userCups=Number(input());
+
 }
 
 //RUN APP:
