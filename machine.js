2003daad12b9267c8e967ec2f059bb5773d3bf84
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-01-24 10:37:55 -0500

    ...

---
+++ machine.js
@@ -0,0 +1,14 @@
+// Use "input()" to input a line from the user
+// Use "input(str)" to print some text before requesting input
+// You will need this in the following stages
+const input = require('sync-input')
+
+function starting(){
+    console.log(`Starting to make a coffee\nGrinding coffee beans\nBoiling water\nMixing boiled water with crushed coffee beans
+ Pouring coffee into the cup
+ Pouring some milk into the cup
+ Coffee is ready!`);
+}
+
+//RUN APP:
+starting();
---
+++ util.js
@@ -0,0 +1,13 @@
+//split:
+let texto = "hello@yoursite.edu.co.mak";
+
+console.log(texto.split("."));
+//Replace:
+
+let data = "A skunk sat on a stun and thunk the stun stumpk, but the stun thunk the skunk stumpk";
+function replaceString(data) {
+    let replacedStr = data.replace(/stun/g, "stump");
+   return replacedStr
+}
+
+console.log(replaceString(data));
