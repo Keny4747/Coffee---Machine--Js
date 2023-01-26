38d7255d9cbbdd536fc7b85a2891d0f20e324abd
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-01-26 10:51:16 -0500

    ...

--- machine.js
+++ machine.js
@@ -1,57 +1,103 @@
-// Use "input()" to input a line from the user
-// Use "input(str)" to print some text before requesting input
-// You will need this in the following stages
 const input = require("sync-input");
 const water = 200;
 const milk = 50;
 const beans = 15;
-
-function starting() {
-  console.log(`Starting to make a coffee\nGrinding coffee beans\nBoiling water\nMixing boiled water with crushed coffee beans
- Pouring coffee into the cup
- Pouring some milk into the cup
- Coffee is ready!`);
+var machine = {
+  water: 400,
+  milk: 540,
+  beans: 120,
+  cups: 9,
+  money: 550,
+};
+function statusCoffeeMachine() {
+  console.log(`The coffee machine has:
+${machine.water} ml of water
+${machine.milk} ml of milk
+${machine.beans} g of coffee beans
+${machine.cups} disposable cups
+$${machine.money} of money`);
 }
-function inputUser() {
+
+function buy() {
+  const espresso = { water: 250, milk: 0, beans: 16, price: 4 };
+  const latte = { water: 350, milk: 75, beans: 20, price: 7 };
+  const capuccino = { water: 200, milk: 100, beans: 12, price: 6 };
+  console.log(
+    "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:"
+  );
+  let userInput = Number(input());
+
+  switch (userInput) {
+    case 1:
+      machine.water -= espresso.water;
+      machine.beans -= espresso.beans;
+      machine.money += espresso.price;
+      machine.cups--;
+      break;
+    case 2:
+      machine.water -= latte.water;
+      machine.milk -= latte.milk;
+      machine.beans -= latte.beans;
+      machine.money += latte.price;
+      machine.cups--;
+      break;
+    case 3:
+      machine.water -= capuccino.water;
+      machine.milk -= capuccino.milk;
+      machine.beans -= capuccino.beans;
+      machine.money += capuccino.price;
+      machine.cups--;
+      break;
+    default:
+      console.log("Invalid input");
+  }
+}
+function menu() {
+  console.log("\nWrite action (buy, fill, take):");
+  let user = String(input());
+  switch (user) {
+    case "buy":
+      buy();
+      statusCoffeeMachine();
+      break;
+    case "fill": //TODO;
+      break;
+    case "take": //TODO;
+      break;
+    default:
+      console.log("Invalid input");
+  }
+}
+function coffeeAvailable() {
+  console.log("Write how many ml of water the coffee machine has:");
+  let userWater = Number(input());
+  console.log("Write how many ml of milk the coffee machine has:");
+  let userMilk = Number(input());
+  console.log("Write how many grams of coffee beans the coffee machine has:");
+  let userBeans = Number(input());
   console.log("Write how many cups of coffee you will need:");
-  let user = Number(input());
-  return user;
+  let userCups = Number(input());
+  cupsOfCoffee(userWater, userMilk, userBeans, userCups);
 }
-function calculateCoffe(inputUser) {
-
-  console.log(`For ${inputUser} cups of coffee you will need:
-${inputUser * water} ml of water
-${inputUser * milk} ml of milk
-${inputUser * beans} g of coffee beans`);
-}
-function coffeeAvailable(){
-    console.log("Write how many ml of water the coffee machine has:");
-    let userWater=Number(input());
-    console.log("Write how many ml of milk the coffee machine has:");
-    let userMilk=Number(input());
-    console.log("Write how many grams of coffee beans the coffee machine has:");
-    let userBeans=Number(input());
-    console.log("Write how many cups of coffee you will need:");
-    let userCups=Number(input());
-
-    
-    cupsOfCoffee(userWater,userMilk,userBeans,userCups);
-}
-function cupsOfCoffee(userWater,userMilk,userBeans, userCups){
+function cupsOfCoffee(userWater, userMilk, userBeans, userCups) {
   var totalW = Math.floor(userWater / water);
   var totalM = Math.floor(userMilk / milk);
   var totalB = Math.floor(userBeans / beans);
-  var total =  Math.min(totalB,Math.min(totalW,totalM));
+  var total = Math.min(totalB, Math.min(totalW, totalM));
 
-  var res = total-userCups;
-  if(total>userCups){
-    console.log(`Yes, I can make that amount of coffee (and even ${res} more than that)`);
-  }else if(total == userCups){
+  var res = total - userCups;
+  if (total > userCups) {
+    console.log(
+      `Yes, I can make that amount of coffee (and even ${res} more than that)`
+    );
+  } else if (total == userCups) {
     console.log("Yes, I can make that amount of coffee");
-  }else{
+  } else {
     console.log(`No, I can make only ${total} cups of coffee`);
   }
 }
 
 //RUN APP:
-coffeeAvailable();
+statusCoffeeMachine();
+menu();
+//coffeeAvailable();
